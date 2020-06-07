2c6b1c43db829ec098c95b8641491e5cd0f1dd27
yunfei <625019880@qq.com>
2020-06-07 23:54:42 +0800

    nats项目提交

---
+++ main/nats.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"../main/utils"
+)
+
+func main() {
+	//向redis写入数据
+	//redis.WriteRedis()
+	//监听nats消息，获取内容后从redis中读取
+	//slice1 := make([] string)
+	var slice = []string{"ff-nats:tag_id:1101", "ff-nats:tag_id:1102", "ff-nats:tag_id:1103"}
+	//strings := append(slice, "ff-nats:tag_id:1101", "ff-nats:tag_id:1102", "ff-nats:tag_id:1103")
+	redis.ReadRedis(slice)
+}
